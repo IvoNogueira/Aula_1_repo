8e08890dc1b2009e3f4e88cedd592c78f333ec5b
Ivo Nogueira <ivo.nogueira@glovoapp.com>
2020-10-02 20:13:55 +0100

    acrescentei tempo frio

--- tempo.py
+++ tempo.py
@@ -1,7 +1,10 @@
 
-def tempo():
-    print('today is a good day')
+def tempo(arg = None):
+    if x < 10:
+        print('today is cold')
+    else:
+        print('today is a good day')
 
 x = input('How hot is today? ')
 
-tempo()
+tempo(x)
